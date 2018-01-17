8b04dd0b813e03e09350bee878873c0b6eae0815
Tyler Garlick <tjgarlick@gmail.com>
2018-01-16 18:48:16 -0700

    Example validation code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log(`hello world 123`)
+import Express from 'express'
+import Parser from 'body-parser'
+// import Joi from 'joi'
+import { joiValidate, Joi } from 'express-joi'
+
+
+
+const app = Express()
+
+app.use(Parser.json())
+
+const loginSchema = {
+  email: Joi.types.String().email().required(),
+  password: Joi.types.String().min(6).max(128).required()
+}
+
+app.post('/login', joiValidate(loginSchema), (req, res) => {
+  const { body } = req
+
+  console.log(body)
+  return res.json(body)
+})
+
+app.get('/', (req, res) => {
+  return res.send('Hello')
+})
+
+app.listen(3000)
+
